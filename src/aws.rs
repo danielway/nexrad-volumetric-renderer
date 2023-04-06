b95dda5be298415624b73e2601b03f9a8834b82f
Daniel Way <projects@danieldway.com>
2023-04-05 20:43:58 -0500

    Index using archive bucket, ignore .idea

--- src/aws.rs
+++ src/aws.rs
@@ -1,18 +1,37 @@
 use aws_config::from_env;
 use aws_sdk_s3::{config::Region, types::Object, Client};
 
-pub async fn get_client() -> Client {
-    let conf = from_env().region(Region::new("us-east-1")).load().await;
-    Client::new(&conf)
+pub async fn list_nexrad(year: &str, month: &str, day: &str, site: &str) -> Result<Vec<String>, aws_sdk_s3::Error> {
+    let prefix = format!("{}/{}/{}/{}", year, month, day, site);
+
+    let client = get_client().await;
+    let objects = list_objects(client, "noaa-nexrad-level2", &prefix)
+        .await
+        .expect("should fetch latest data")
+        .expect("should have objects");
+
+    Ok(objects
+        .iter()
+        .map(|obj| {
+            let mut result = String::new();
+            result.push_str(obj.key().unwrap());
+            result.push_str(" - ");
+            result.push_str(&obj.last_modified().unwrap().to_millis().unwrap().to_string());
+
+            return result;
+        })
+        .collect())
 }
 
 pub async fn list_objects(
     client: Client,
     bucket: &str,
+    prefix: &str,
 ) -> Result<Option<Vec<Object>>, aws_sdk_s3::Error> {
     Ok(client
         .list_objects_v2()
         .bucket(bucket)
+        .prefix(prefix)
         .customize()
         .await
         .unwrap()
@@ -22,6 +41,11 @@ pub async fn list_objects(
         .await?
         .contents()
         .map(|objects| objects.to_vec()))
+}
+
+pub async fn get_client() -> Client {
+    let conf = from_env().region(Region::new("us-east-1")).load().await;
+    Client::new(&conf)
 }
 
 // Disables signing requirements for unauthenticated S3 requests.
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-use aws_sdk_s3::config::Region;
-
 mod aws;
-use crate::aws::{get_client, list_objects};
+use crate::aws::{list_nexrad};
 
 #[tokio::main]
 async fn main() {
-    let client = get_client().await;
-    let objects = list_objects(client, "unidata-nexrad-level2-chunks")
+    let objects = list_nexrad("2023", "04", "06", "KDMX")
         .await
-        .expect("should fetch latest data")
-        .expect("should have objects");
+        .expect("should fetch latest data");
 
     for obj in objects {
-        println!("{}", obj.key().unwrap_or_default());
+        println!("{}", obj);
     }
 }
