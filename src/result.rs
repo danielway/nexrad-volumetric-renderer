81d10eb48b8037099be1ff4ee0e2ee158417ab32
Daniel Way <projects@danieldway.com>
2023-04-15 10:35:13 -0500

    Structure NEXARD decoder, begin M31 routineˆ

--- src/result.rs
+++ src/result.rs
@@ -1,9 +1,15 @@
 pub type Result<T> = std::result::Result<T, Error>;
 
+pub fn unexpected_error(message: &str) -> Error {
+    Error::Unexpected(message.to_string())
+}
+
 #[derive(Debug)]
 pub enum Error {
+    Unexpected(String),
     Utf8Error(std::str::Utf8Error),
     FileError(std::io::Error),
+    BinaryDeserializationError(bincode::Error),
     S3GeneralError(aws_sdk_s3::Error),
     S3ListObjectsError(aws_smithy_http::result::SdkError<
         aws_sdk_s3::operation::list_objects_v2::ListObjectsV2Error
@@ -22,6 +28,12 @@ impl From<std::str::Utf8Error> for Error {
 impl From<std::io::Error> for Error {
     fn from(err: std::io::Error) -> Self {
         Error::FileError(err)
+    }
+}
+
+impl From<bincode::Error> for Error {
+    fn from(err: bincode::Error) -> Self {
+        Error::BinaryDeserializationError(err)
     }
 }
 
