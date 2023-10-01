fe3ba1f916f7d125b6924c377ee98e323dc2ac77
Daniel Way <projects@danieldway.com>
2023-10-01 08:48:32 -0500

    Add scaled earth

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use chrono::{NaiveDate, NaiveTime};
 use kiss3d::light::Light;
-use kiss3d::nalgebra::{UnitQuaternion, Vector3};
+use kiss3d::nalgebra::{Point3, Translation3, UnitQuaternion, Vector3};
 use kiss3d::window::Window;
 use nexrad::decode::decode_file;
 use nexrad::decompress::decompress_file;
@@ -21,13 +21,22 @@ async fn main() {
     execute(TARGET_SITE, &target_date, &target_time).await.unwrap();
 }
 
+const EARTH_RADIUS_M: f64 = 6356752.3;
+const RENDER_RATIO_TO_M: f64 = 0.001; // every 1.0 in the render == 1.0/RENDER_RATIO_TO_M meters
+
 async fn execute(site: &str, date: &NaiveDate, time: &NaiveTime) -> Result<()> {
     let mut window = Window::new("NEXRAD Volumetric Renderer");
 
+    let earth_scaled_radius = (EARTH_RADIUS_M * RENDER_RATIO_TO_M) as f32;
+    let mut earth = window.add_sphere(earth_scaled_radius);
+    earth.set_local_translation(Translation3::new(0.0, -earth_scaled_radius, 0.0));
+    earth.set_color(82.0/255.0, 143.0/255.0, 79.0/255.0);
+
     let mut cube = window.add_cube(1.0, 1.0, 1.0);
     cube.set_color(1.0, 0.0, 0.0);
+    cube.set_local_translation(Translation3::new(0.0, 1.0, 0.0));
 
-    window.set_light(Light::StickToCamera);
+    window.set_light(Light::Absolute(Point3::new(0.0, 100.0, 0.0)));
 
     let rotation = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
 
