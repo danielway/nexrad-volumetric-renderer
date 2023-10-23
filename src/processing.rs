3c2db643b4f44d7cd16f67daaef2afe96ac0820d
Daniel Way <projects@danieldway.com>
2023-10-22 22:02:02 -0500

    Disable clustering, recolor normally

--- src/processing.rs
+++ src/processing.rs
@@ -41,46 +41,46 @@ pub async fn fetch_and_process(
     // todo: we need to weight and rescale geometrically before clustering
     // todo: in addition to result/density, weight should consider gate distance
 
-    println!("Clustering points...");
-    let (clusters, unclustered) = do_dbscan_clustering(sampled_points.clone());
-    println!(
-        "Found {} clusters with {} remaining unclustered points.",
-        clusters.len(),
-        unclustered.len()
-    );
+    // println!("Clustering points...");
+    // let (clusters, unclustered) = do_dbscan_clustering(sampled_points.clone());
+    // println!(
+    //     "Found {} clusters with {} remaining unclustered points.",
+    //     clusters.len(),
+    //     unclustered.len()
+    // );
 
     // todo: coloring clusters to debug
 
-    let mut index = 0.0;
-    let mut get_color = || {
-        let phi = (1.0 + 5.0_f64.sqrt()) / 2.0;
-        let n = index * phi - (index * phi).floor();
-        let color = HSL {
-            h: n * 256.0,
-            s: 1.0,
-            l: 0.5,
-        };
-        index += 1.0;
-        return color.to_rgb();
-    };
-
-    println!("Recoloring clusters for debugging.");
-    let mut recolored_points = Vec::new();
-    let unclustered_color = (255, 255, 255);
-    for mut point in unclustered {
-        point.1 = unclustered_color;
-        recolored_points.push(point);
-    }
-    for cluster in clusters {
-        let cluster_color = get_color();
-        for mut point in cluster {
-            point.1 = cluster_color;
-            recolored_points.push(point);
-        }
-    }
+    // let mut index = 0.0;
+    // let mut get_color = || {
+    //     let phi = (1.0 + 5.0_f64.sqrt()) / 2.0;
+    //     let n = index * phi - (index * phi).floor();
+    //     let color = HSL {
+    //         h: n * 256.0,
+    //         s: 1.0,
+    //         l: 0.5,
+    //     };
+    //     index += 1.0;
+    //     return color.to_rgb();
+    // };
+    //
+    // println!("Recoloring clusters for debugging.");
+    // let mut recolored_points = Vec::new();
+    // let unclustered_color = (255, 255, 255);
+    // for mut point in unclustered {
+    //     point.1 = unclustered_color;
+    //     recolored_points.push(point);
+    // }
+    // for cluster in clusters {
+    //     let cluster_color = get_color();
+    //     for mut point in cluster {
+    //         point.1 = cluster_color;
+    //         recolored_points.push(point);
+    //     }
+    // }
 
     let mut state = state.lock().unwrap();
-    state.points = Some(recolored_points);
+    state.points = Some(sampled_points);
     state.processing = false;
 
     println!("Done fetch/processing!");
